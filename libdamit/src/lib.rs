9cfa42c061a9d03d27c82677b4aabb73a0983bd6
Ivan Boldyrev <lispnik@gmail.com>
2021-08-21 19:48:26 +0300

    Find INTERP segment

--- libdamit/src/lib.rs
+++ libdamit/src/lib.rs
@@ -1,7 +1,7 @@
-use std::path::PathBuf;
-
 use elfloader::ElfBinary;
 use loader::DamLoader;
+use log::{debug, info};
+use std::error::Error;
 
 mod loader;
 
@@ -15,14 +15,23 @@ pub struct Binary {
 }
 
 impl Binary {
-    pub fn new(data: Vec<u8>) -> Self {
+    pub fn new(data: Vec<u8>) -> Result<Self, Box<dyn Error>> {
         let mut bin = Binary {
             segments: Default::default(),
         };
         let binary = ElfBinary::new(&data).expect("Got proper ELF file");
+        let mut interp = None;
+        for ph in binary.program_headers() {
+            debug!("header: {:?}", ph);
+            if matches!(ph.get_type(), Ok(xmas_elf::program::Type::Interp)) {
+                interp = Some(ph.get_data(&binary.file)?);
+                info!("interp: {:?}", interp.unwrap());
+            }
+        }
+
         let mut loader = DamLoader { bin: &mut bin };
         binary.load(&mut loader).expect("Can't load the binary?");
-        bin
+        Ok(bin)
     }
 }
 
