4850d0a324ba7c875fcb45e193a0fe6ed4df406a
Ivan Boldyrev <lispnik@gmail.com>
2021-08-24 21:09:57 +0300

    libdamit: Add BinaryError with thiserror
    Get rid of the Box<dyn Error>.

--- libdamit/src/lib.rs
+++ libdamit/src/lib.rs
@@ -2,8 +2,17 @@ use elfloader::ElfBinary;
 use loader::DamLoader;
 use log::{debug, info};
 use std::{error::Error, ffi::CString, iter::FromIterator};
+use thiserror::Error;
 
 mod loader;
+
+#[derive(Debug, Error)]
+pub enum BinaryError {
+    #[error("Invalid Interp: {:?}", .0)]
+    InvalidInterp(Vec<u8>),
+    #[error("{}", .0)]
+    Str(&'static str),
+}
 
 struct Segment {
     headers: Vec<elfloader::ProgramHeader64>,
@@ -16,7 +25,7 @@ pub struct Binary {
 }
 
 impl Binary {
-    pub fn new(data: Vec<u8>) -> Result<Self, Box<dyn Error>> {
+    pub fn new(data: Vec<u8>) -> Result<Self, BinaryError> {
         let mut bin = Binary {
             segments: Default::default(),
             interp: None,
@@ -25,15 +34,22 @@ impl Binary {
         for ph in binary.program_headers() {
             debug!("header: {:?}", ph);
             if matches!(ph.get_type(), Ok(xmas_elf::program::Type::Interp)) {
-                let data = ph.get_data(&binary.file)?;
+                let data = ph.get_data(&binary.file).map_err(BinaryError::Str)?;
                 debug!("interp data: {:?}", data);
                 match data {
                     xmas_elf::program::SegmentData::Undefined(dat) => {
+                        // There is an unstable CString::from_vec_with_nul method:
+                        // https://github.com/rust-lang/rust/issues/73179
+                        // We should use it eventually.
                         let mut buf = Vec::from_iter(dat.into_iter().copied());
                         if matches!(buf.last(), Some(0)) {
                             buf.pop();
                         }
-                        let interp_result = CString::new(buf)?;
+                        let interp_result = CString::new(buf)
+                            // FIXME: this is not the original value, and
+                            // it can be misleading.  We should use the unstable
+                            // function above.
+                            .map_err(|e| BinaryError::InvalidInterp(e.into_vec()))?;
                         info!("interp: {:?}", interp_result);
                         bin.interp = Some(interp_result);
                     }
